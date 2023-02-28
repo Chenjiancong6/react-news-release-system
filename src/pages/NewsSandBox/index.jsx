5428507639bb1be08cb9afda2a0c1a45853eda01
chenjiancong <chenjiancong@sutpc.com>
2023-02-28 18:32:23 +0800

    feat: 添加页面请求的进度条

--- src/pages/NewsSandBox/index.jsx
+++ src/pages/NewsSandBox/index.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+// useEffect 在渲染后执行某些操作,并且在执行 DOM 更新之后调用它
+import React,{ useEffect } from "react"
 //progress主要应用的场景为页面切换和请求数据，在进行这两项操作时浏览器的顶部会出现进度条，优化用户体验
 import NProgress from "nprogress"
 import { Layout } from 'antd';
@@ -10,6 +11,11 @@ import NewsRouter from "./NewsRouter"
 const { Content } = Layout;
 
 export default function NewsSandBox() {
+    NProgress.start()
+    useEffect(()=> {
+        NProgress.done()
+    })
+    
     return (
         <Layout>
             <Layout className="site-layout">
