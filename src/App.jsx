349fa9605cff48af7be5b09d0de89bbad9e139b0
chenjiancong <chenjiancong@sutpc.com>
2023-03-08 18:47:23 +0800

    feat: redux 持久化

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import IndexRouter from "./routers/IndexRouter"
 // Provider 包裹在路由里，实现路由内任意通信
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 import "./App.css"
 
 function App() {
     return (
         <Provider store={store}>
-            <IndexRouter></IndexRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <IndexRouter></IndexRouter>
+            </PersistGate>
         </Provider>
     )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,16 @@
 import {createStore} from 'redux'
-
 import reducers from './reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-export  let store = createStore(reducers)
+//redux 持久化,将redux保存到本地
+const persistConfig = {
+    key: 'redux-persist',
+    storage,
+    whitelist: ['isCollapsedReducers'] // 白名单
+}
+
+const persistedReducer = persistReducer(persistConfig,reducers)
+
+export  let store = createStore(persistedReducer)
+export  let persistor = persistStore(store)
